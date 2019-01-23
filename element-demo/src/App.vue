539fa905f7ca0830d9c2dca73934fce5d736c7bb
ningxiaoxia@huawei.com <920322606@qq.com>
2019-01-23 23:01:54 +0800

    Signed-off-by: ningxiaoxia@huawei.com <920322606@qq.com>

---
+++ element-demo/src/App.vue
@@ -0,0 +1,57 @@
+<template>
+  <div id="app">
+    <h1>{{ msg }}</h1>
+    <button>sdfsfd</button>
+    <el-button type="primary">主要按钮</el-button>
+    <el-button type="success" icon="serach">主要按钮</el-button>
+    <span class="el-icon-delete"></span>
+    <el-row>
+      <el-col :span="12">
+        <div class="my-grid">34</div>
+      </el-col>
+      <el-col :span="12">
+         <div class="my-grid">ss</div>
+      </el-col>
+    </el-row>
+      <el-row>
+        <el-col :span="8">
+          <div class="my-grid">34</div>
+        </el-col>
+        <el-col :span="8">
+          <div class="my-grid">ss</div>
+        </el-col>
+        <el-col :span="8">
+          <div class="my-grid">
+          <Date></Date>
+          </div>
+        </el-col>
+    </el-row>
+  </div>
+</template>
+
+<script>
+import Date from './components/Date.vue'
+export default {
+  name: 'app',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  },
+  components:{
+    Date
+  }
+}
+</script>
+
+<style scoped lang="less">
+@color:red;
+.height(@h){
+  height:@h;
+}
+.my-grid{
+  border:1px solid @color;
+  
+  .height(50px);
+}
+</style>
---
+++ element-demo/src/components/Date.vue
@@ -0,0 +1,25 @@
+<template>
+   <div id="date">
+        <el-date-picker 
+        v-model="value1"
+        type="date"
+        placeholder="选择日期"
+        :picker-options="ranger">
+        </el-date-picker>
+   </div>
+</template>
+<script>
+export default{
+    date(){
+        return {
+            value1:'',
+            ranger:{
+                disabledDate(time){
+                    return time.getTime() < Date.now() - 8.64e7;
+
+                }
+            }
+        }
+    }
+}
+</script>
